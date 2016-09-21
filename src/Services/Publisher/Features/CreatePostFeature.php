a988272a133202efcea93c0546b65d08fd44c6e5
Charalampos Raftopoulos <harrisrafto@gmail.com>
2016-09-21 20:33:24 +0300

    add create post feature boilerplate

---
+++ src/Services/Publisher/Features/CreatePostFeature.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Services\Publisher\Features;
+
+use Lucid\Foundation\Feature;
+use Illuminate\Http\Request;
+
+class CreatePostFeature extends Feature
+{
+    public function handle(Request $request)
+    {
+
+    }
+}
---
+++ src/Services/Publisher/Tests/Features/CreatePostFeatureTest.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Services\Publisher\Tests\Features;
+
+use TestCase;
+use App\Services\Publisher\Features\CreatePostFeature;
+
+class CreatePostFeatureTest extends TestCase
+{
+    public function test_createpostfeature()
+    {
+        $this->markTestIncomplete();
+    }
+}
